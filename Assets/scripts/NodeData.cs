39b86adc08cd60ea022fc1a68e46036b97ccacfc
Tymko Tecca <tymko.tecca@gmail.com>
2014-03-06 12:19:42 -0500

    Beginning of node system. Merging back so I can take Zhan's guard and incorporate it properly.

---
+++ Assets/scripts/NodeData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeData : MonoBehaviour {
+
+	public bool SuperNode = false;
+	public bool lastNode = false;
+
+	void onTriggerEnter (Collider other){
+		if(other.GetComponent<
+	}
+
+}
