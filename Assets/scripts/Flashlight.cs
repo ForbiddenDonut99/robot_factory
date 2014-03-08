16586393eb0defacfd67164a0cf7fdc3b1534714
forbiddendonut <dillon.aron@gmail.com>
2014-03-08 13:32:54 -0500

    pushed new prefabs - use the ones with the 'pf' prefix included is a new 4-sided room
    also organized the scripts folder a bit

---
+++ Assets/scripts/Flashlight.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+	public Light flashlightSpotlight;
+	public bool flashlightToggle;
+	public bool preventRefire = false;
+
+	void Start() {
+		flashlightSpotlight.enabled = false;
+
+		}
+
+	void FixedUpdate() {
+
+		if (Input.GetKey (KeyCode.F) && preventRefire == false) {
+				StartCoroutine("FlashlightCoroutine");
+
+
+				}
+
+		}
+
+
+
+	private IEnumerator FlashlightCoroutine() {
+		if (flashlightToggle == false) {
+					preventRefire = true;
+					flashlightSpotlight.enabled = true;
+					flashlightToggle = true;
+					yield return new WaitForSeconds(1);
+					preventRefire = false;
+				} else {
+					preventRefire = true;
+					flashlightSpotlight.enabled = false;
+					flashlightToggle = false;
+					yield return new WaitForSeconds(1);
+					preventRefire = false;
+		}
+
+		
+	}
+
+		
+
+
+
+
+}
