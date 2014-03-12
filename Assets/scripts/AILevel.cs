8503549b2d3f20123c1480b368e3774dec7befb1
forbiddendonut <dillon.aron@gmail.com>
2014-03-12 16:13:08 -0400

    +additional furniture models +powerups now have models +WIP menu screen

---
+++ Assets/scripts/AILevel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AILevel : MonoBehaviour {
+
+	public GUIText startText;
+
+	void Start () {
+
+		StartCoroutine ("startTextCor");
+		}
+
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.R)) {
+
+			Application.LoadLevel("PopulationWorkspace");
+		}
+	}
+
+	IEnumerator startTextCor() {
+		yield return new WaitForSeconds (5);
+		startText.enabled = false;
+
+	}
+
+
+}
