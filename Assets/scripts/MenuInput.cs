385faa27fdec52e74e239bf02cbe01b90faf8b97
Krosantos <tymko.tecca@gmail.com>
2014-03-13 01:43:41 -0400

    Guards have their flashlights back, menu has its code back.

--- Assets/scripts/MenuInput.cs
+++ Assets/scripts/MenuInput.cs
@@ -2,22 +2,74 @@
 using System.Collections;
 
 public class MenuInput : MonoBehaviour {
-
-	public KeyCode buttonToPush = KeyCode.Space;
-	public string nextLevel = "TymkoGuard";
-
-	// Use this for initialization
-	void Start () {
 	
+	public string MainScene;
+	public string OptionsMenu;
+	public GameObject Line1;
+	public GameObject Line2;
+	public GameObject Line3;
+	public GameObject Line4;
+	public Material whiteText;
+	public Material yellowText;
+	int state = 0;
+	
+	void Start () {
+		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
-		if (Input.GetKeyDown (buttonToPush)) {
-
-			Application.LoadLevel(nextLevel);
+		
+		//Move the selector.
+		if(Input.GetKeyDown(KeyCode.DownArrow)&& state < 3){
+			state++;
 		}
-	
+		else if (Input.GetKeyDown(KeyCode.UpArrow) && state > 1){
+			state--;
+		}
+		
+		//Do something once you select an option.
+		if(Input.GetKeyDown(KeyCode.Space)||Input.GetKeyDown(KeyCode.Return)){
+			switch(state){
+			case 1:
+				Application.LoadLevel(MainScene);
+				break;
+				
+			case 2:
+				Application.LoadLevel (OptionsMenu);
+				break;
+				
+			case 3:
+				Application.Quit();
+				break;
+			}
+		}
+		
+		//Switch the text color to indicate selection.
+		switch(state){
+		case 0:
+			Line1.renderer.material = yellowText;
+			Line2.renderer.material = whiteText;
+			Line3.renderer.material = whiteText;
+			Line4.renderer.material = whiteText;
+			break;
+		case 1:
+			Line1.renderer.material = whiteText;
+			Line2.renderer.material = yellowText;
+			Line3.renderer.material = whiteText;
+			Line4.renderer.material = whiteText;
+			break;
+		case 2:
+			Line1.renderer.material = whiteText;
+			Line2.renderer.material = whiteText;
+			Line3.renderer.material = yellowText;
+			Line4.renderer.material = whiteText;
+			break;
+		case 3:
+			Line1.renderer.material = whiteText;
+			Line2.renderer.material = whiteText;
+			Line3.renderer.material = whiteText;
+			Line4.renderer.material = yellowText;
+			break;
+		}
 	}
 }
