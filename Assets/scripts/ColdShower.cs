f1974ed1690572e69c6c906b8c62969e77a11d53
Tymko Tecca <tymko.tecca@gmail.com>
2014-03-17 13:03:33 -0400

    Coroutine to fix frozen guards/guard orgies.

---
+++ Assets/scripts/ColdShower.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColdShower : MonoBehaviour {
+
+	Vector3 record = new Vector3(0,0,0);
+
+	void Start (){
+		StartCoroutine (coldShower());
+	}
+	/*void Update(){
+		if (transform.position == record){
+			gameObject.GetComponent<TrialPatrol>().state = "MoveToNode";
+			gameObject.GetComponent<TrialPatrol>().targetNode = gameObject.GetComponent<TrialPatrol>().lastNode;
+			Debug.Log ("No Homo, bro.");
+		}
+		StartCoroutine (delayTracking());
+	}*/
+
+	IEnumerator coldShower(){
+		for(;;){
+			if (transform.position == record){
+				gameObject.GetComponent<TrialPatrol>().state = "MoveToNode";
+				gameObject.GetComponent<TrialPatrol>().targetNode = gameObject.GetComponent<TrialPatrol>().lastNode;
+				Debug.Log ("No Homo, bro.");
+			}
+			record = transform.position;
+			Debug.Log ("Position Updated!");
+			yield return new WaitForSeconds(1.5f);
+		}
+	}
+
+	/*IEnumerator delayTracking(){
+		yield return new WaitForSeconds(2);
+		record = transform.position;
+		Debug.Log ("waited");
+		yield return new WaitForSeconds(2);
+	}*/
+
+	/*void OnTriggerEnter(Collider other){
+		if (other.tag == "Guard"){
+			other.GetComponent<TrialPatrol>().targetNode = other.GetComponent<TrialPatrol>().lastNode;
+			other.GetComponent<TrialPatrol>().state = "FindNode";
+			Debug.Log ("No homo, bro.");
+		}
+	}*/
+
+}
