1da0f6eff7dacd2cf17940f1a4582ac028b5ce50
Krosantos <tymko.tecca@gmail.com>
2014-03-22 23:25:25 -0400

    Liiiitle more cleanup.

--- Assets/scripts/ColdShower.cs
+++ Assets/scripts/ColdShower.cs
@@ -8,40 +8,15 @@ public class ColdShower : MonoBehaviour {
 	void Start (){
 		StartCoroutine (coldShower());
 	}
-	/*void Update(){
-		if (transform.position == record){
-			gameObject.GetComponent<TrialPatrol>().state = "MoveToNode";
-			gameObject.GetComponent<TrialPatrol>().targetNode = gameObject.GetComponent<TrialPatrol>().lastNode;
-			Debug.Log ("No Homo, bro.");
-		}
-		StartCoroutine (delayTracking());
-	}*/
 
 	IEnumerator coldShower(){
 		for(;;){
 			if (transform.position == record){
 				gameObject.GetComponent<TrialPatrol>().state = "MoveToNode";
 				gameObject.GetComponent<TrialPatrol>().targetNode = gameObject.GetComponent<TrialPatrol>().lastNode;
-				//Debug.Log ("No Homo, bro.");
 			}
 			record = transform.position;
 			yield return new WaitForSeconds(0.2f);
 		}
 	}
-
-	/*IEnumerator delayTracking(){
-		yield return new WaitForSeconds(2);
-		record = transform.position;
-		Debug.Log ("waited");
-		yield return new WaitForSeconds(2);
-	}*/
-
-	/*void OnTriggerEnter(Collider other){
-		if (other.tag == "Guard"){
-			other.GetComponent<TrialPatrol>().targetNode = other.GetComponent<TrialPatrol>().lastNode;
-			other.GetComponent<TrialPatrol>().state = "FindNode";
-			Debug.Log ("No homo, bro.");
-		}
-	}*/
-
 }
