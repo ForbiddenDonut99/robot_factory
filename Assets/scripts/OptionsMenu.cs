67ffd15f05644d1348288d00bd4391f92ca559a7
Zhan Huang <vh.noir@gmail.com>
2014-03-25 18:56:35 -0400

    deleted some old scripts, made menus accept mouse input instead

--- Assets/scripts/OptionsMenu.cs
+++ Assets/scripts/OptionsMenu.cs
@@ -2,155 +2,120 @@
 using System.Collections;
 
 public class OptionsMenu : MonoBehaviour {
-
-	public static bool fullscreen = true;
+	
+	public static bool isFullscreen = false;
 	public static int width = 1280;
 	public static int height = 1024;
 	public static float sfx = 1f;
-	int state = 0;
 	string ratio = "(5:4)";
-	public GameObject Line1;
-	public GameObject Line2;
-	public GameObject Line3;
-	public GameObject Line4;
-	public GameObject resolution;
+	public GameObject Resolution;
 	public GameObject Fullscreen;
 	public GameObject Volume;
+	public GameObject Back;
 	public Material whiteText;
 	public Material yellowText;
+	public Material orangeText;
 
 	void Update () {
 
-		//Move the selector.
-		if(Input.GetKeyDown(KeyCode.DownArrow)&& state < 3){
-			state++;
-		}
-		else if (Input.GetKeyDown(KeyCode.UpArrow) && state > 0){
-			state--;
-		}
-
 		//Update the lines pertaining to changeable options.
-		resolution.GetComponent<TextMesh>().text = width + "*" + height + "  " + ratio;
-		Volume.GetComponent<TextMesh>().text = (sfx*10).ToString("#.")+"0%";
-		if(fullscreen){
-			Fullscreen.GetComponent<TextMesh>().text = "[x]";
+		Resolution.GetComponent<TextMesh>().text = "Resolution      " + width + "*" + height + " " + ratio;
+		Volume.GetComponent<TextMesh>().text = "Volume      " + (sfx*10).ToString("#.")+"0%";
+		if(isFullscreen){
+			Fullscreen.GetComponent<TextMesh>().text = "Fullscreen      [x]";
 		}
 		else{
-			Fullscreen.GetComponent<TextMesh>().text = "[ ]";
+			Fullscreen.GetComponent<TextMesh>().text = "Fullscreen      [  ]";
 		}
 
-		//Shit happens when you hit da buttons.
-		if(Input.GetKeyDown(KeyCode.Space)||Input.GetKeyDown(KeyCode.Return)){
-			switch(state){
-			case 0:
-				switch(width){
-				case 1920:
-					width = 640;
-					height = 480;
-					ratio = "(4:3)";
-					break;
-				case 640:
-					width = 800;
-					height = 600;
-					ratio = "(4:3)";
-					break;
-				case 800:
-					width = 1024;
-					height = 768;
-					ratio = "(4:3)";
-					break;
-				case 1024:
-					width = 1280;
-					height = 1024;
-					ratio = "(5:4)";
-					break;
-				case 1280:
-					width = 1440;
-					height = 900;
-					ratio = "(16:10)";
-					break;
-				case 1440:
-					width = 1680;
-					height = 1050;
-					ratio = "(16:10)";
-					break;
-				case 1680:
-					width = 1600;
-					height = 900;
-					ratio = "(16:9)";
-					break;
-				case 1600:
-					width = 1920;
-					height = 1080;
-					ratio = "(16:9)";
-					break;
-				}
-				break;
+		// reset color
+		Resolution.renderer.material = whiteText;
+		Fullscreen.renderer.material = whiteText;
+		Volume.renderer.material = whiteText;
+		Back.renderer.material = whiteText;
 
-			case 1:
-				if(fullscreen){
-					fullscreen = false;
+		// mouse controls
+		Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit rayHit = new RaycastHit();
+		if(Physics.Raycast(mouseRay, out rayHit, 100f)){
+			GameObject option = rayHit.transform.gameObject;
+			if(option.tag == "Option"){
+				if(Input.GetMouseButtonDown(0)){
+					// clicked on option
+					if(option == Resolution){
+						Resolution.renderer.material = yellowText;
+						switch(width){
+						case 1920:
+							width = 640;
+							height = 480;
+							ratio = "(4:3)";
+							break;
+						case 640:
+							width = 800;
+							height = 600;
+							ratio = "(4:3)";
+							break;
+						case 800:
+							width = 1024;
+							height = 768;
+							ratio = "(4:3)";
+							break;
+						case 1024:
+							width = 1280;
+							height = 1024;
+							ratio = "(5:4)";
+							break;
+						case 1280:
+							width = 1440;
+							height = 900;
+							ratio = "(16:10)";
+							break;
+						case 1440:
+							width = 1680;
+							height = 1050;
+							ratio = "(16:10)";
+							break;
+						case 1680:
+							width = 1600;
+							height = 900;
+							ratio = "(16:9)";
+							break;
+						case 1600:
+							width = 1920;
+							height = 1080;
+							ratio = "(16:9)";
+							break;
+						}
+					} else if(option == Fullscreen){
+						Fullscreen.renderer.material = yellowText;
+						isFullscreen = !isFullscreen;
+					} else if(option == Volume){
+						Volume.renderer.material = yellowText;
+						if(sfx >= 1f){
+							sfx = 0f;
+						}
+						else{
+							sfx += 0.1f;
+						}
+					} else if(option == Back){
+						Back.renderer.material = orangeText;
+						Screen.SetResolution(width, height, isFullscreen);
+						Application.LoadLevel("MainMenu");
+					}
+				} else{
+					// highlight the option
+					if(option == Resolution){
+						Resolution.renderer.material = yellowText;
+					} else if(option == Fullscreen){
+						Fullscreen.renderer.material = yellowText;
+					} else if(option == Volume){
+						Volume.renderer.material = yellowText;
+					} else if(option == Back){
+						Back.renderer.material = yellowText;
+					}
+					
 				}
-				else{
-					fullscreen = true;
-				}
-				break;
-				
-			case 2:
-				if(sfx >= 1f){
-					sfx = 0f;
-				}
-				else{
-					sfx += 0.1f;
-				}
-				break;
-				
-			case 3:
-				Screen.SetResolution(width, height, fullscreen);
-				Application.LoadLevel("MainMenu");
-				break;
 			}
 		}
-
-		//Switch the text color to indicate selection.
-		switch(state){
-		case 0:
-			Line1.renderer.material = yellowText;
-			resolution.renderer.material = yellowText;
-			Line2.renderer.material = whiteText;
-			Fullscreen.renderer.material = whiteText;
-			Line3.renderer.material = whiteText;
-			Volume.renderer.material = whiteText;
-			Line4.renderer.material = whiteText;
-			break;
-		case 1:
-			Line1.renderer.material = whiteText;
-			resolution.renderer.material = whiteText;
-			Line2.renderer.material = yellowText;
-			Fullscreen.renderer.material = yellowText;
-			Line3.renderer.material = whiteText;
-			Volume.renderer.material = whiteText;
-			Line4.renderer.material = whiteText;
-			break;
-		case 2:
-			Line1.renderer.material = whiteText;
-			resolution.renderer.material = whiteText;
-			Line2.renderer.material = whiteText;
-			Fullscreen.renderer.material = whiteText;
-			Line3.renderer.material = yellowText;
-			Volume.renderer.material = yellowText;
-			Line4.renderer.material = whiteText;
-			break;
-		case 3:
-			Line1.renderer.material = whiteText;
-			resolution.renderer.material = whiteText;
-			Line2.renderer.material = whiteText;
-			Fullscreen.renderer.material = whiteText;
-			Line3.renderer.material = whiteText;
-			Volume.renderer.material = whiteText;
-			Line4.renderer.material = yellowText;
-			break;
-		}
 	}
-	
 }
