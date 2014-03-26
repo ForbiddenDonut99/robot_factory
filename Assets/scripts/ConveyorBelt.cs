e00813b0c3a61a6fecadb860844399785539c02e
Zhan Huang <vh.noir@gmail.com>
2014-03-26 13:10:33 -0400

    conveyorbelt now moves player

---
+++ Assets/scripts/ConveyorBelt.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorBelt : MonoBehaviour {
+
+	public float beltSpeed = 2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay (Collider other) {
+		if(other.tag == "Player"){
+			Transform playerTransform = other.transform;
+			playerTransform.position = new Vector3(playerTransform.position.x,playerTransform.position.y,playerTransform.position.z + Time.deltaTime * beltSpeed);
+		}
+	}
+}
