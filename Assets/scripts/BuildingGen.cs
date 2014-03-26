fbc6f199ae1dfafc32a860bef83b56d914137b71
Zhan Huang <vh.noir@gmail.com>
2014-03-26 03:09:50 -0400

    load springboard, also restored furniture generation rate

--- Assets/scripts/BuildingGen.cs
+++ Assets/scripts/BuildingGen.cs
@@ -47,7 +47,7 @@ public class BuildingGen : MonoBehaviour {
 	GameObject machinePreFab;
 	GameObject shelfPreFab;
 	GameObject node;
-	public float furnitureChance = 28.57f;
+	public float furnitureChance = 45f;
 	public float machineRoomChance = 30f;
 	
 	// Use this for initialization
@@ -60,7 +60,7 @@ public class BuildingGen : MonoBehaviour {
 		powerupFlashlight = (GameObject)Resources.Load("Powerup-Flashlight");
 		powerupStungun = (GameObject)Resources.Load("Powerup-Stungun");
 		powerupCompass = (GameObject)Resources.Load("Powerup-Compass");
-		powerupSpring = (GameObject)Resources.Load("Powerup-Compass");
+		powerupSpring = (GameObject)Resources.Load("Powerup-Spring");
 		guard = (GameObject)Resources.Load("Guard");
 		
 		player = (GameObject)Resources.Load("PlayerRobot");
